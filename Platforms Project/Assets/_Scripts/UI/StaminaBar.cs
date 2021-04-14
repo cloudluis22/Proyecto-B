12393dfb18d1229ec393b3f31248c2edb559be25
José Luis Ávila Juárez <cloudluis22@hotmail.com>
2021-04-14 13:06:48 -0500

    Stamina mejorada.

--- Platforms Project/Assets/_Scripts/UI/StaminaBar.cs
+++ Platforms Project/Assets/_Scripts/UI/StaminaBar.cs
@@ -7,15 +7,21 @@ public class StaminaBar : MonoBehaviour
 {
 
     [SerializeField] private Slider staminaSlider;
+    [SerializeField] private Image sliderFill;
+    
+    [SerializeField] private Color sliderColor, cooldownColor;
     [Range(1f, 50f)] [SerializeField] private float lossFrequency, gainFrequency;
 
     private GameObject player;
     private PlayerController playerController;
 
+    private bool cooldown;
 
-    private void Start() {
+    private void Start() 
+    {
         player = GameObject.FindGameObjectWithTag("Player");
         playerController = player.GetComponent<PlayerController>();
+        sliderFill.color = Color.blue;
     }
 
     private void StaminaStatus()
@@ -27,6 +33,22 @@ public class StaminaBar : MonoBehaviour
         else
         {
             staminaSlider.value += Time.deltaTime * lossFrequency;
+        }
+
+        if(staminaSlider.value == 0)
+        {
+            cooldown = true;
+        }
+
+        if(cooldown)
+        {
+            sliderFill.color = Color.red;
+
+            if(staminaSlider.value > staminaSlider.maxValue / 4)
+            {
+                cooldown = false;
+                sliderFill.color = Color.blue;
+            }
         }
     }
 
